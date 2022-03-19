92bbfe8180ee83fdd84524dc8b3b2b10d4c4e2f0
Shiva Karka <64298475+Shivakarka@users.noreply.github.com>
2022-03-19 15:21:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tooltip from "./Tooltip";
+import "./App.css"; 
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React tooltip</h1>
+      <p> Try hovering the content below </p>
+
+      <div className="wrapper">
+      {/* position prop can be top,right,left,bottom || default is top*/}
+        <Tooltip position="right">          
+          <button className="content">
+            Hover over me!!
+          </button>
+        </Tooltip>      
+         
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Tooltip.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./Tooltip.css";
+
+const Tooltip = ({children,position}) => {
+  let timeout;
+  const [active, setActive] = useState(false);      
+
+  //Below setTimeout function changes the state to true after a delay as specified in 2nd argument
+  //in absence of 2nd argument , the tooltip display will be instant
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, 200);
+  };
+
+  //Below function clears the timeout from above and changes the state back to false
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className="Tooltip-Wrapper"
+
+      // The below two eventListeners control when to display the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+       
+      {/* {the children prop here is the button where we need to hover upon} */}
+      
+      {children}
+
+      {/* The tooltip is displayed only when the setActive state is true */}
+      {active && (
+        <div className={`Tooltip-Tip ${position || "top"}`}>
+
+          {/* Tooltip content */}
+          {`This is a ${position || "top"} tooltip!!!`}
+        </div>
+      )}
+
+    </div>
+  );
+};
+
+export default Tooltip;
